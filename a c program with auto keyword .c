dc403802fe802f182c8a815542403a86f6a983eb
Durga-ai910 <durga57141@gmail.com>
2024-08-01 13:49:21 +0530

    Create a c program with auto keyword .c

---
+++ a c program with auto keyword .c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int fun()
+{
+    int a=14;
+    printf("%d",a);
+    return 0;
+}
+int main()
+{
+    auto int a,b;
+    a=4;
+    fun();
+    printf("\n%d",a);
+    return 0;
+}
