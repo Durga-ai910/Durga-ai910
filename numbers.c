df688c0f24988ef5c9cd576384138530d142cabc
Durga-ai910 <durga57141@gmail.com>
2024-12-02 11:36:59 +0530

    Add files via upload

---
+++ numbers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n;
+    printf("enter an integer");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        printf("%d",i);
+    }
+    return 0;
+}
